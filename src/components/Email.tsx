48fa821441344a2645f66aa656c4db0db6257e46
ashkan <ashkanpc2003@gmail.com>
2024-02-29 20:18:44 +0330

    create email-list step-2 finish

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,11 +1,18 @@
 import { emailType } from "../types/globalTypes";
-import "./Email.css"
+import "./Email.css";
 interface Props {
   email: emailType;
 }
 
 const Email = ({ email }: Props) => {
-  return <div className="email-container">Email</div>;
+  return (
+    <div className="email-container">
+      <img src={email.avatar}/>
+      <span className="email-name">{email.name}</span>
+      <span className="email-des">{email.des}</span>
+      <span className="email-date">{email.date}</span>
+    </div>
+  );
 };
 
 export default Email;
--- src/components/EmailsList.tsx
+++ src/components/EmailsList.tsx
@@ -4,37 +4,37 @@ import "./EmailsList.css"
 // this is a simple data-base for emails list
 const emailListArray: emailType[] = [
   {
-    avatar: "",
+    avatar: "/public/avatar-img/avatar-1.jpg",
     name: "Livia Stanton",
     des: "Meeting scheduled",
     date: "12:14 PM",
   },
   {
-    avatar: "",
+    avatar: "/public/avatar-img/avatar-2.jpg",
     name: "Allison Lipshutz",
     des: "Update on marketing campaign",
     date: "Des 19 at 8:01 PM",
   },
   {
-    avatar: "",
+    avatar: "/public/avatar-img/avatar-3.jpg",
     name: "Martin Stanton",
     des: "Designly 2.0 is about to launch",
     date: "Des 19 at 6:11 PM",
   },
   {
-    avatar: "",
+    avatar: "/public/avatar-img/avatar-4.jpg",
     name: "Alfredo Botosh",
     des: "My friend Julie loves Dapper!",
     date: "Des 19 at 6:11 PM",
   },
   {
-    avatar: "",
+    avatar: "/public/avatar-img/avatar-5.jpg",
     name: "Ann Philips",
     des: "My friend Julie loves Dapper!",
     date: "Des 19 at 7:11 PM",
   },
   {
-    avatar: "",
+    avatar: "/public/avatar-img/avatar-6.jpg",
     name: "Allison Lipshuts",
     des: "Update on marketing",
     date: "Des 19 at 8:01 PM",
