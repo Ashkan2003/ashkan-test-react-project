6c17db00b0cdf2f93b23025231c206b5826d82cc
ashkan <ashkanpc2003@gmail.com>
2024-03-03 10:42:00 +0330

    create menu step-2 (finish)

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -14,23 +14,40 @@ const menuList: menuLiType[] = [
   { title: "Group", icon: "/public/svgs/people.svg" },
   { title: "List", icon: "/public/svgs/list.svg" },
   { title: "Finance", icon: "/public/svgs/money.svg" },
+  { title: "Setting", icon: "/public/svgs/settings.svg" },
 ];
 
 const Menu = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
-
+  const handleClick = () => {
+    setMenuOpen((isOpen) => !isOpen);
+  };
   return (
-    <div className="menu">
-      <p className="title">Drop</p>
-      {/* menu list */}
-      <ul>
-        {menuList.map((item, index) => (
-          <li key={index}>
-            <img id="icon" src={item.icon} alt="icon" />
-            {/* <span>{item.title}</span> */}
-          </li>
-        ))}
-      </ul>
+    <div className="menu-container">
+      <div className={`menu ${isMenuOpen && "menu-open"}`}>
+        <p className="title">Drop</p>
+        {/* menu-toggle-btn */}
+        <button onClick={handleClick} className="menu-toggle-btn">
+          <img
+            className={`${isMenuOpen && "rotated"}`}
+            src="/public/svgs/right-arrow.svg"
+            alt=""
+          />
+        </button>
+        {/* menu list */}
+        <ul className="list">
+          {menuList.map((item, index) => (
+            <li key={index}>
+              <span className="prefix-item"></span>
+              <img src={item.icon} alt="icon" />
+              <span className="menu-list-name fadeInUp-animation">
+                {item.title}
+              </span>
+              <span className="list-name-button-box fadeInUp-animation"></span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
